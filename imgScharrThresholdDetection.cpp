8af305f96179ca5dae360fa3ea5940d273dc7b57
yeshg <yesh.godse@gmail.com>
2016-01-01 09:51:38 -0800

    Added Scharr threshold edge detection

--- imgScharrThresholdDetection.cpp
+++ imgScharrThresholdDetection.cpp
@@ -1,4 +1,4 @@
-// Program to detect edges in an image using the Scharr operator
+// Program to detect edges in an image using the thresholded Scharr operator
 // Author: Samarth Manoj Brahmbhatt, University of Pennyslvania
 
 #include <opencv2/opencv.hpp>
@@ -7,8 +7,20 @@
 
 using namespace std;
 using namespace cv;
-/*
+
+Mat edges, edges_thresholded;
+int slider = 50;
+
+void on_slider(int, void *) {
+    if(!edges.empty()) {
+        Mat edges_thresholded;
+        threshold(edges, edges_thresholded, slider, 255, THRESH_TOZERO);
+        imshow("Thresholded Scharr edges", edges_thresholded);
+    }
+}
+
 int main() {
+    //Mat image = imread("lena.jpg"), image_blurred;
     Mat image = imread("lena.jpg"), image_blurred;
 
     // Blur image with a Gaussian kernel to remove edge noise
@@ -16,7 +28,7 @@ int main() {
 
     // Convert to gray
     Mat image_gray;
-    cvtColor(image_blurred, image_gray, CV_RGB2GRAY);
+    cvtColor(image_blurred, image_gray, CV_BGR2GRAY);
 
     // Gradients in X and Y directions
     Mat grad_x, grad_y;
@@ -32,18 +44,19 @@ int main() {
     sqrt(grad, grad);
 
     // Display
-    namedWindow("Original image");www
-    namedWindow("Scharr edges");
+    namedWindow("Original image");
+    namedWindow("Thresholded Scharr edges");
 
     // Convert to 8 bit depth for displaying
-    Mat edges;
     grad.convertTo(edges, CV_8U);
+    threshold(edges, edges_thresholded, slider, 255, THRESH_TOZERO);
 
     imshow("Original image", image);
-    imshow("Scharr edges", edges);
+    imshow("Thresholded Scharr edges", edges_thresholded);
+
+    createTrackbar("Threshold", "Thresholded Scharr edges", &slider, 255, on_slider);
 
     while(char(waitKey(1)) != 'q') {}
 
     return 0;
 }
-*/
